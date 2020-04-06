50d66fa11719b43a0a3a6deca6610007809a575a
Victoria <vicky.pizzorno1@gmail.com>
2020-04-05 22:19:26 -0300

    atributos de Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,9 @@
+public class Usuario {
+    private String nombre;
+    private String cedula;
+    private String password;
+    private int edad;
+    private int cant_hijos;
+    private float saldo = 0;
+
+}
