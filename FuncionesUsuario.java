bb40ee64d79ba65f52c6a027e16767a135aed231
Victoria <vicky.pizzorno1@gmail.com>
2020-04-05 23:48:16 -0300

    commit calculo imp

---
+++ FuncionesUsuario.java
@@ -0,0 +1,12 @@
+import Usuario;
+
+public class FuncionesUuario{
+    private float CalculoIngresos(Usuario u){
+        resultado res;
+        if u.getEdad() < 18 {
+            res=0;
+        }
+
+    }
+    private float CobroSueld{}
+}
