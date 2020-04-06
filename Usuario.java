f740cda46a7a97b308b9e55a8ce66ac304683166
Victoria <vicky.pizzorno1@gmail.com>
2020-04-05 23:01:59 -0300

    commit con getters y setters

--- Usuario.java
+++ Usuario.java
@@ -4,6 +4,49 @@ public class Usuario {
     private String password;
     private int edad;
     private int cant_hijos;
-    private float saldo = 0;
+    private float saldo;
 
-}
+    public Usuario(String nom, String cedId, String pass, int anios, int hijos){
+        this.nombre=nom;
+        this.cedula=cedId;
+        this.password=pass;
+        this.edad=anios;
+        this.cant_hijos=hijos;
+        this.saldo=0;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getCedula(){
+        return cedula;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public int getCantHijos(){
+        return cant_hijos;
+    }
+    public float getSaldo(){
+        return saldo;
+    }
+    public void setNombre(String nom){
+        this.nombre=nom;
+    }
+    public void setCedula(String CId){
+        this.cedula=CId;
+    }
+    public void setPassword(String contra){
+        this.password=contra;
+    }
+    public void setEdad(int anios){
+        this.edad=anios;
+    }
+    public void setCantHijos(int cantidad){
+        this.cant_hijos=cantidad;
+    }
+    public void setSaldo(float saldo){
+        this.saldo=saldo;
+    }
